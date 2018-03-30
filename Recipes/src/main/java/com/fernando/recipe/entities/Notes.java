62e4af4cdbd8c60a469bd26800048f9d50823c90
Fernando Viel <nandoviel@hotmail.com>
2018-03-30 10:25:54 -0300

    anotação de atributos na notes

--- Recipes/src/main/java/com/fernando/recipe/entities/Notes.java
+++ Recipes/src/main/java/com/fernando/recipe/entities/Notes.java
@@ -5,10 +5,29 @@
  */
 package com.fernando.recipe.entities;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
 /**
  *
  * @author fviel
  */
+@Entity
+@Table(name = "tb_notes", schema = "recipe")
 public class Notes {
+
+    @Id
+    @SequenceGenerator(name = "NOTES_ID_SEQ", sequenceName = "tb_notes_id_seq", schema = "recipe", allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "NOTES_ID_SEQ")
+    @Column(name = "id_notes")
+    private int idNotes;
+
+    @Column(name = "notes")
+    private String notes;
 
 }
