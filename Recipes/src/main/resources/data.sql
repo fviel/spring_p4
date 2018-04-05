19428486ca4e41f703d4dd44d9bbeecbe6ae14b9
Fernando Viel <nandoviel@hotmail.com>
2018-04-04 22:08:02 -0300

    Criei arquivo data.sql, onde defini alguns inserts padrões.

---
+++ Recipes/src/main/resources/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO tb_category (id_category, department_name) VALUES(0,'American');
+INSERT INTO tb_category (id_category, department_name) VALUES(1,'Italian');
+INSERT INTO tb_category (id_category, department_name) VALUES(2,'Brazilian');
+INSERT INTO tb_category (id_category, department_name) VALUES(3, 'Mexican');
+INSERT INTO tb_uom(id_uom, uom) VALUES(0, 'Teaspoon');
+INSERT INTO tb_uom(id_uom, uom) VALUES(1, 'Cup');
+INSERT INTO tb_uom(id_uom, uom) VALUES(2, 'Pinch');
+INSERT INTO tb_uom(id_uom, uom) VALUES(3, 'Ounce');
+
