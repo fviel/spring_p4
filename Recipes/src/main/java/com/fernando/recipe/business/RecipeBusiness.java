1d54295fc1167541e4a647ca2ab653a28f30de3a
Fernando Viel <nandoviel@hotmail.com>
2018-04-05 23:01:52 -0300

    Criada a estrutura para o exercício das receitas

---
+++ Recipes/src/main/java/com/fernando/recipe/business/RecipeBusiness.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.fernando.recipe.business;
+
+import com.fernando.recipe.entities.Recipe;
+import java.util.List;
+
+/**
+ *
+ * @author Fernando
+ */
+public class RecipeBusiness {
+    
+    
+}
---
+++ Recipes/src/main/java/com/fernando/recipe/services/RecipeService.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.fernando.recipe.services;
+
+import com.fernando.recipe.entities.Recipe;
+import java.util.List;
+
+/**
+ *
+ * @author Fernando
+ */
+public class RecipeService {
+    public void createGuacaMoleRecipe(){
+        //cria no bd e entidades a receita de guacamole
+    }
+    
+    public void createSpicyChickenRecipe(){
+        //cria no bd e entidades a receita de spicy grilled chicken
+    }
+    
+    public List<Recipe> getRecipes(){
+        //retorna a lista de receitas pesistidas na base de dados
+        return null;
+    }
+    
+    public Recipe getRecipe(int id){
+        return null;
+    }
+}
